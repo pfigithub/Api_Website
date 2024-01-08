c293be9f2dc483e4edeed2fa13d9340a3a979ea4
pfigithub <pampam7072@gmail.com>
2024-01-08 15:40:31 +0330

    Update admin.py
    create custom user admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import User, Profile
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "is_staff", "is_active", "is_verified")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        ("Authentication", {"fields": ("email",)}),
+        (
+            "Permissions",
+            {"fields": ("is_superuser", "is_staff", "is_active", "is_verified")},
+        ),
+        (
+            "Group Permissions",
+            {
+                "fields": (
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important_date", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_superuser",
+                    "is_staff",
+                    "is_active",
+                    "is_verified",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(Profile)
+admin.site.register(User, CustomUserAdmin)
