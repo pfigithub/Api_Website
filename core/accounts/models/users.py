7be0d8aff7a1c48af402f69815ba425284e3df88
pfigithub <pampam7072@gmail.com>
2024-01-13 16:57:58 +0330

    password_reset
    password reset class and migrations

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -59,3 +59,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+class PasswordReset(models.Model):
+    email = models.EmailField()
+    token = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
