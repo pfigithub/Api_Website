476d22223fadc973c1a3eb5dcdc5a6c7d19f444c
pfigithub <pampam7072@gmail.com>
2024-01-14 15:47:23 +0330

    Update models.py
    create post and category model and migrate

--- core/blog/models.py
+++ core/blog/models.py
@@ -2,6 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class Post(models.Model):
+    """this is a class to define posts for blog app"""
+     
+    author = models.ForeignKey("accounts.Profile", on_delete = models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    title = models.CharField(max_length= 250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey("Category", on_delete= models.SET_NULL, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
 
 
+    def __str__(self):
+        return self.title
 
+
+class Category(models.Model):
+    """class for category"""
+
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
