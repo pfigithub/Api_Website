4ffdc47c59d2589a0541b27ed60bfc8f7df75255
pfigithub <pampam7072@gmail.com>
2024-01-18 17:56:41 +0330

    Create urls.py
    create and update urls for website app

---
+++ core/website/urls.py
@@ -0,0 +1,22 @@
+from website.views import *
+from django.urls import path
+
+app_name = 'web'
+
+urlpatterns = [
+    path("fbvi/", FunctionBaseViewIndex, name="fbv-index"),
+    # manual templateview
+    path("cbvi/", C1assBaseViewIndex.as_view(), name="cbv-index"),
+    # auto templateview
+    path("cbtv/", TemplateView.as_view(template_name="web/index.html", extra_context ={'whatever':'hello template view'})),
+    # cbv for contact page
+    path('contact/', ContactFormView.as_view(), name = 'contact'),
+    # fbv for contact page
+    # path('contact/', contact, name = 'contact'),
+    # url for cbv
+    path("about/", AboutView.as_view(), name = 'about'),
+    # url for fbv
+    # path("about/", about, name = 'about'),
+    path('newsletter/', newsletter_view, name = 'newsletter'),
+    path("redirect/", RedirectToEveryWhere.as_view(), name="redirect"),
+]
