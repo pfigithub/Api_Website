4e6d0eb50776ffa06bb8be56a6495ebfb534c212
pfigithub <pampam7072@gmail.com>
2024-01-18 17:44:07 +0330

    Update urls.py
    move to main web app

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -9,12 +9,6 @@ app_name = "blog"
 
 urlpatterns = [
     #path("api/v1", include("blog.api.v1.urls")),
-    path("fbvi", views.FunctionBaseViewIndex, name="fbv-index"),
-    # manual templateview
-    path("cbvi", views.C1assBaseViewIndex.as_view(), name="cbv-index"),
-    # auto templateview
-    path("cbtv", TemplateView.as_view(template_name="index.html", extra_context ={'whatever':'hello template view'})),
-    path("redirect", views.RedirectToEveryWhere.as_view(), name="redirect"),
     path("post/", views.PostListView.as_view(), name="post-list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("post/create/", views.PostCreateView.as_view(), name="post-create"),
