b33f2f75703ee479d7acaa64ff0c505d7a2089a9
pfigithub <pampam7072@gmail.com>
2024-01-18 17:42:26 +0330

    Create forms.py
    model forms for contact and newsletter model

---
+++ core/website/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from website.models import Contact, Newsletter
+#from captcha.fields import CaptchaField
+
+class ContactForm(forms.ModelForm):
+    #captcha = CaptchaField()
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+    # def send_email(self):
+    #     # send email using the self.cleaned_data dictionary
+    #     pass
+
+
+class NewsletterForm(forms.ModelForm):
+
+    class Meta:
+        model = Newsletter
+        fields = '__all__'
