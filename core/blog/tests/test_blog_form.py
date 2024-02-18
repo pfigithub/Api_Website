63f4c6e07514b65b6015e6990501c735d85cefe3
pfigithub <pampam7072@gmail.com>
2024-02-18 18:03:44 +0330

    Create test_blog_form.py
    test for postform

---
+++ core/blog/tests/test_blog_form.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from datetime import datetime
+from ..forms import PostForm
+from ..models import Category
+
+class TestPostForm(TestCase):
+
+    def test_post_form_with_valid_data(self):
+        category_obj = Category.objects.create(name = 'cat1')
+        form = PostForm(data= {
+            "title":"test",
+            "content":"description",
+            "status":True,
+            "category":category_obj,
+            "published_date":datetime.now()
+        })
+        self.assertTrue(form.is_valid())
