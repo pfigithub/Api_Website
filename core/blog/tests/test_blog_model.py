49edd1c25c71464b934c413c10bbd6f38027414f
pfigithub <pampam7072@gmail.com>
2024-02-18 18:04:18 +0330

    Create test_blog_model.py
    test for model and blog post

---
+++ core/blog/tests/test_blog_model.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from datetime import datetime
+
+from accounts.models import User, Profile
+from ..models import Post
+
+class TestPostModel(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(email='test60@test.com', password='a/@1234567')
+        self.profile = Profile.objects.create(
+            user = self.user,
+            first_name = 'first_name_test',
+            last_name = 'last_name_test',
+            description = 'description_test'
+        )
+
+    
+    def test_create_post_with_valid_data(self):
+        post = Post.objects.create(
+            author = self.profile,
+            title= "another test",
+            content= "description",
+            status= True,
+            category= None,
+            published_date= datetime.now()
+        )
+        self.assertTrue(Post.objects.filter(pk=post.id).exists())
+        self.assertEquals(post.title, "another test")
